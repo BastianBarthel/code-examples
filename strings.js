43d6b28ac7567f16ea29ff6870deb8f0f98690b8
Bastian Barthel <barthel.esq@gmail.com>
2023-11-20 16:03:38 +0100

    Update strings.js

--- strings.js
+++ strings.js
@@ -9,3 +9,18 @@ console.log(airline.slice(4, 7));
 console.log(airline.slice(-1)); // last character
 console.log(airline.slice(0, airline.indexOf(" "))); // first word
 console.log(airline.slice(airline.lastIndexOf(" ") + 1)); // last word
+
+// Changing Case
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Get rid of leading and trailing white space
+console.log(airline.trim());
+
+// Replace Character
+console.log(plane.replace("0", "1")); // replaceAll is now included in JS!
+
+// Booleans
+console.log(plane.includes("A32"));
+console.log(plane.startsWith("B"));
+console.log(plane.endsWith("20"));
