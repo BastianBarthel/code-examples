aedbc8fd86668ba766f56efbb20213f554687060
Bastian Barthel <barthel.esq@gmail.com>
2023-11-24 14:57:23 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -37,3 +37,15 @@ const transformer = function (str, fn) {
 
 transformer("JavaScript is the best!", upperFirstWord);
 transformer("JavaScript is the best!", oneWord);
+
+// Functions returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}.`);
+  };
+};
+
+const greeterHello = greet("Hello");
+greeterHello("Bastian");
+
+greet("Hey")("Jude");
